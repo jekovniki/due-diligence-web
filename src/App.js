877c75775e2de3b418baa52ec2b70e2f1fae478f
nikolayz <nikolay.zhekov@onseostudio.com>
2023-06-20 09:28:33 +0300

    Minor adjustments for the new backend design

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   const [pepList, setPepList] = useState([]);
   const [status, setStatus] = useState('Loading...');
   const [searchStatus, setSearchStatus] = useState('');
-  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/v1/pep').then(response => {
@@ -22,10 +21,11 @@ function App() {
 
   const getPerson = (event) => {
     if (!event.target.value) {
+      setSearchStatus("");
       return "";
     }
     const result = pepList.filter(person => {
-      if (person.Name && person.Name.includes(event.target.value)) {
+      if (person.name && person.name.includes(event.target.value)) {
         return person;
       }
 
@@ -50,11 +50,24 @@ function App() {
             let index = 0;
             for(const person of searchStatus) {
               allResults.push(
-              <div id={index} style={{borderBottom: "1px solid #ededed", marginBottom: "1rem"}}>
-                <div><p><strong>ИМЕ: </strong> {person.Name}</p></div>
-                <div><p><strong>ПОЗИЦИЯ: </strong> {person.Position}</p></div>
-                <div><p><strong>КАТЕГОРИЯ: </strong> {person.Category}</p></div>
-                <div><p><strong>ИНСТИТУЦИЯ: </strong> {person.Institution}</p></div>
+              <div id={index} style={{borderBottom: "1px solid black", margin: "1rem", paddingBlockEnd: "1rem"}}>
+                <div><h2><strong>ИМЕ: </strong> {person.name}</h2></div>
+                <div style={{display: "grid", gap: "50px", gridTemplateColumns: "1fr 1fr 1fr 1fr"}}>
+                { (() => {
+                  const jobInformation = [];
+                  let index = 0;
+                  for (const job of person.history) {
+                    jobInformation.push(<div id={index} style={{border: "1px solid red", display: "flex", flexFlow: "column"}}>
+                      <p><strong>ГОДИНА: { job.year }</strong></p>
+                      <p><strong>ЗАЕМАНА ПОЗИЦИЯ: { job.position }</strong></p>
+                      <p><strong>КАТЕГОРИЯ НА ПОЗИЦИЯТА: { job.category }</strong></p>
+                      <p><strong>ИНСТИТУЦИЯ НА РАБОТА: { job.institution }</strong></p>
+                    </div>)
+                  }
+
+                  return jobInformation;
+                })()}
+                </div>
               </div>)
 
               index++;
